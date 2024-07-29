b4a8b28e2237f9bff9b66a65f8cd073710b92d94
Bogdan Kovalev <bogdan.kovalev.job@gmail.com>
2024-07-29 15:59:53 +0100

    Fix exchangeInfo req limit

--- fetchBinance.ts
+++ fetchBinance.ts
@@ -1,43 +1,46 @@
 import { APIGatewayEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const handler = async (event: APIGatewayEvent): Promise<APIGatewayProxyResult> => {
-    console.log(`Event: ${JSON.stringify(event, null, 2)}`);
-
     const resource = (event as any).rawPath ?? `ping`;
     const query = (event as any).rawQueryString ?? ``;
     const method = (event.requestContext as any).http.method;
-    const url = `https://api.binance.com/api/v3${resource}?${query}`;
+    const url = `https://api.binance.com/api/v3${resource}${query ? `?${query}` : ''}`;
 
     console.log(method, url);
 
-    const binanceRequest = fetch(url, {
-        method,
-        headers: {
-            'x-mbx-apikey': (event.headers?.[`x-mbx-apikey`] as string) ?? ``,
-        },
-    });
-
-    return await binanceRequest
-        .then(async (binanceResponse) => {
-            return await binanceResponse
-                .text()
-                .then((text) => {
-                    return {
-                        statusCode: binanceResponse.status,
-                        body: text,
-                    };
-                })
-                .catch((error) => {
-                    return {
-                        statusCode: 500,
-                        body: error,
-                    };
-                });
-        })
-        .catch((error) => {
-            return {
-                statusCode: 500,
-                body: error,
-            };
+    try {
+        const response = await fetch(url, {
+            method,
+            headers: {
+                'x-mbx-apikey': (event.headers?.[`x-mbx-apikey`] as string) ?? ``,
+            },
         });
+
+        let respText = '';
+        if (resource.includes('exchangeInfo')) {
+            const exInfo = await response.json();
+            exInfo.symbols = exInfo.symbols.map((s: any) => {
+                return {
+                    symbol: s.symbol,
+                    status: s.status,
+                    filters: s.filters,
+                };
+            });
+            respText = JSON.stringify(exInfo);
+        } else {
+            respText = await response.text();
+        }
+        console.log(response.status, respText.slice(0, 1000));
+
+        return {
+            statusCode: response.status,
+            body: respText,
+        };
+    } catch (e) {
+        console.error(e);
+        return {
+            statusCode: 500,
+            body: e as string,
+        };
+    }
 };
